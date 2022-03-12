0dc2b02b962caac8566b441190754fc5c0e005c1
sanjay <ssah6694@gmail.com>
2022-03-12 23:20:15 +0530

    [sanjay]Added; power of Two Problem3

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number here");
+            int num=int.Parse(Console.ReadLine());
+            int value = PowerOf2(num);
+            Console.WriteLine(value);    
+        }
+        public static int PowerOf2(int n)
+        {
+            int result = 1;
+            while(n>0)
+            {
+                result = result * 2;
+                n--;
+            }
+            return result;
+        }
+    }
+}
