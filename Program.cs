98bc6756909610f924c83a4ae56df283cfec2ab3
sanjay <ssah6694@gmail.com>
2022-03-12 23:36:45 +0530

    [sanjay]Added;Harmonic Number Problem4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double value=HarmonicNumber(4);
+            Console.WriteLine(value);
+        }
+        public static double HarmonicNumber(int n)
+        {
+            double result = 0;
+            for(double i=1;i<=n;i++)
+            {
+                result = result + 1 / i;
+            }
+            return result;
+        }
+    }
+}
