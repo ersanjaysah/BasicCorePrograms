2bbf4e95c0aa81a48431685d09530b47f046f89b
sanjay <ssah6694@gmail.com>
2022-03-13 00:09:54 +0530

    [sanjay]Added;Swap Two number Problem8

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EvenAndOddNumber
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the number");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(" This is a Even number");
+            }
+            else
+            {
+                Console.WriteLine("This is an odd number");
+            }
+       
+
+        }
+
+    }
+}
