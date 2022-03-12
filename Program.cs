39e2202cef351aa64387c37e53d819799ceecfe2
sanjay <ssah6694@gmail.com>
2022-03-12 23:45:05 +0530

    [sanjay]Added: Factorial of a number

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FactorsOfANumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number");
+            int num = int.Parse(Console.ReadLine());
+            Program.MyMethod(num);
+        }
+
+        public static void MyMethod(int num)
+            {
+                int i = 2;
+
+                for (i = 2; i > 1; i++)
+                {
+
+                    while (num % i == 0)
+                    {
+                        Console.WriteLine("prime factors are"i);
+                        num = num / i;
+                    }
+
+                }
+            }
+        
+    }
+}
