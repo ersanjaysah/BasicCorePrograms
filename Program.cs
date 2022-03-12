511f377964910ecc0e23d4f9a4ffaf93dee36110
sanjay <ssah6694@gmail.com>
2022-03-13 00:18:40 +0530

    [sanjay]Added; Vowel or Consonent Problem9

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace VowelAndConsonent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input=(Console.ReadLine());
+            char alphabet=Convert.ToChar(input.ToUpper());
+            switch(alphabet)
+            {
+                case 'A':
+                    Console.WriteLine("vowel");
+                    break;
+
+                case 'E':
+                    Console.WriteLine("vowel");
+                    break;
+
+                case 'I':
+                    Console.WriteLine("vowel");
+                    break;
+
+                case 'O':
+                    Console.WriteLine("vowel");
+                    break;
+
+                case 'U':
+                    Console.WriteLine("vowel");
+                    break;
+
+                default:
+                    Console.WriteLine("consonent");
+                    break;
+
+            }
+        }
+    }
+}
