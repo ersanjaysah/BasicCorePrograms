e38f4908a88accd6bb07e1847b57adf07120fd29
sanjay <ssah6694@gmail.com>
2022-03-12 23:57:35 +0530

    [sanjay]Added; Quotient And Remainder Problem6

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace QuotientAndRemainder
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int num=int.Parse(Console.ReadLine());
+            Program.quotient(num);
+
+            Program.remainder(num);
+           
+       
+        }
+        public static void quotient(int num)
+        {
+            int result = 0;
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    result = num / i;
+                    Console.WriteLine(num+"/"+i+" quotient is= "+result);
+                }
+            }
+            
+        }
+        public static void remainder(int num)
+        {
+            int result = 0;
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i != 0)
+                {
+                    result = num % i;
+                    Console.WriteLine(num+"%"+i+" Remainder is= " + result);
+                }
+            }
+        }
+         
+
+     
+
+    }
+}
